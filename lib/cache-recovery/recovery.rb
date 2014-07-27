e19e91afbe0cae7f6f621a6ba3d98ca553d00c0b
Francis Chong <francis@ignition.hk>
2014-07-27 12:33:26 +0800

    Step 1: list all pages from google

--- lib/cache-recovery/recovery.rb
+++ lib/cache-recovery/recovery.rb
@@ -1,17 +1,25 @@
-require "cache/recovery/version"
-
+require 'google-search'
+require 'pry'
 module CacheRecovery
-  # list all links from google of the supplied domain
-  def self.list(domain)
-    pages = []
-    Google::Search::Web.new(:query => "site:#{domain}").each do |result|
-      pages << {
-        :title => result.title,
-        :result => result.uri,
-        :content => result.content,
-        :cache_uri => result.cache_uri
+  class Recovery
+    # list all links from google of the supplied domain
+    # domain - the domain to search for
+    def self.list(domain)
+      options = {
+        :size => :large,
+        :query => "site:#{domain}"
       }
+
+      search = Google::Search::Web.new(options)
+      search.collect do |result|
+        {
+          :title => result.title,
+          :result => result.uri,
+          :content => result.content,
+          :cache_uri => result.cache_uri,
+          :index => result.index
+        }
+      end
     end
-    pages
   end
 end
